1e1e321673460c797c41d228c08a686ec356ee93
Freest10 <powerandfit@yandex.ru>
2017-07-29 17:52:32 +0300

    Edit README

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,168 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  private inctanceOfWidgetsSchema: any;
+  arr: any;
+  changedData: any;
+  submitedData: any;
+  schemas: any = [
+    {
+      name: "simple",
+      value: require("./jsons-of-schema/simple.json")
+    },
+    {
+      name: "array",
+      value: require("./jsons-of-schema/array.json")
+    },
+    {
+      name: "tabs",
+      value: require("./jsons-of-schema/tabs.json")
+    },
+    {
+      name: "tab-array",
+      value: require("./jsons-of-schema/tab-array.json")
+    },
+    {
+      name: "select(object data output)",
+      value: require("./jsons-of-schema/select-object-output.json")
+    },
+    {
+      name: "select(string data output)",
+      value: require("./jsons-of-schema/select-string-output.json")
+    },
+    {
+      name: "integer and radios",
+      value: require("./jsons-of-schema/integer-and-radios.json")
+    },
+    {
+      name: "list(object output)",
+      value: require("./jsons-of-schema/list-object-output.json")
+    },
+    {
+      name: "list(string output)",
+      value: require("./jsons-of-schema/list-string-output.json")
+    },
+    {
+      name: "validating",
+      value: require("./jsons-of-schema/validating.json")
+    },
+    {
+      name: "bootstrap grid",
+      value: require("./jsons-of-schema/bootstrap-grid.json")
+    }
+  ]
+  frameWorkName = "material-design";
+  frameWorksName = [{name: "noFrameWork", value: "none"}, {name: "Material Design", value: "material-design"}];
+
+  selectedSchema: any;
+
+  aceEditorOptions: any = {
+    highlightActiveLine: true,
+    maxLines: 1000,
+    printMargin: false,
+    autoScrollEditorIntoView: true
+  };
+
+  options = {
+   // "apiUrl": "api",
+    "frameWorkName": 'material-design'
+  }
+
+  // model: any = {
+  //   "last_name": "Имзя"
+  // };
+  constructor(){}
+
+  ngOnInit(){
+    this.selectedSchema = this.schemas[0];
+  }
+
+  changedSelectSchema(event){
+    let changedSchemaName = event.value;
+    this.selectSchemaByName(changedSchemaName);
+  }
+
+
+
+  selectSchemaByName(changedSchemaName){
+    this.schemas.forEach((schema)=>{
+      if(schema.name == changedSchemaName){
+        this.selectedSchema = Object.assign({},schema);
+        return false;
+      }
+    })
+  }
+
+  inctanceOfWidgets(widgets){
+    this.inctanceOfWidgetsSchema = widgets;
+    /*this.inctanceOfWidgetsSchema["name"].updateDelay();
+    this.inctanceOfWidgetsSchema["tabContent"].widgetSubject.subscribe((type) => {
+      console.log(type, this.inctanceOfWidgetsSchema["tabContent"], this.inctanceOfWidgetsSchema, 'tabSub');
+    })*/
+    /* this.inctanceOfWidgetsSchema["send"].widgetSubject.subscribe((type) => {
+      if('buttonClick') this.fnctionClikc()
+    })*/
+
+    //widgets["last_name"].updateDelay();
+    /*widgets["send"].buttonClick().subscribe(
+     value => {console.log('next Button Click')},
+     error => console.log('error Button Click'),
+     () => console.log('finished Button Click')
+     )*/
+  }
+
+  inctanceOfWidgets2(widgets){
+    //widgets["first_name"].updateDelay();
+    /*widgets["send"].buttonClick().subscribe(
+     value => {console.log('next Button Click')},
+     error => console.log('error Button Click'),
+     () => console.log('finished Button Click')
+     )*/
+  }
+
+  fnctionClikc(){
+    console.log('fnctionClikc');
+  }
+
+  getValueToTextEditor(){
+    if(this.selectedSchema){
+      return  JSON.stringify(this.selectedSchema.value, null, '\t');
+    }
+  }
+
+  generateForm(value){
+    this.selectedSchema.value = JSON.parse(value);
+  }
+
+  clearChangeDataAndSubmitted(){
+    this.submitedData = "";
+    this.changedData = "";
+  }
+
+  schemaChanges(value){
+    let stringValue = JSON.stringify(value, null, '\t');
+    if(stringValue){
+      this.changedData = stringValue;
+    }else{
+      this.changedData = "";
+    }
+  }
+
+  submitedValue(value){
+    if(value){
+      this.submitedData = JSON.stringify(value, null, '\t');
+    }else{
+      this.submitedData = "";
+    }
+  }
+
+  changedFrameworkSchema(event){
+    this.frameWorkName = event.value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {JsonSchemaViewModule} from "angular4-json-schema-view"
 import { AppComponent } from './app.component';
+import { WidgetsService } from 'angular4-json-schema-view';
+import { JsonSchemaShareOptionsService } from 'angular4-json-schema-view';
+import { CustomInputComponent } from './custom_widgets/custom-input.component';
+import {  MdSelectModule, MdButtonModule, MdCheckboxModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { AceEditorModule } from 'ng2-ace-editor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    JsonSchemaViewModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdSelectModule,
+    FormsModule,
+    AceEditorModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [CustomInputComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private widgetService: WidgetsService, private schemaShareOptions: JsonSchemaShareOptionsService) {
+    //this.widgetService.registerWidget('string', CustomInputComponent);
+    // let shareOptions = {
+    //   "apiUrl": "api2",
+    //   "frameWorkName": 'material-design'
+    // }
+   // this.schemaShareOptions.setShareOptions(shareOptions);
+  }
+
+}
